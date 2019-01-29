348cce889693a5675a15db5f8f71b5e72dcf852a
Ashley Kelly <a.j.kelly@durham.ac.uk>
2019-01-29 12:08:07 +0000

    upadted setup to ensure cython builds extensions correctly

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ import sys
 
 try:
     import numpy as np
+    import cython
 except ImportError:
-    raise ImportError("Please install numpy before building pyquad!")
+    raise ImportError("Please install numpy and cython before building pyquad!")
 
 link_args = ["-std=c99"]
 
+# detect if openmp was passed, if so enable and strip
 if '--openmp' in sys.argv:
     sys.argv.remove('--openmp')
     link_args.append('-fopenmp')
@@ -20,24 +22,35 @@ src_files = ["pyquad/pyquad.pyx", "pyquad/integration/error.c",
              "pyquad/integration/workspace.c"
              ]
 
+ext_modules=[Extension("pyquad",
+             src_files,
+             extra_compile_args=link_args,
+             extra_link_args=link_args,
+             include_dirs=[np.get_include()],
+             language='c')]
+
+# Cythonize our modules
+from Cython.Build import cythonize
+ext_modules = cythonize(ext_modules,
+                        compiler_directives={'language_level': 3})
+
 setup(
     name='pyquad',
     version='0.0.3.5',
     author="Ashley J Kelly",
+    license="LGPL-3.0",
     author_email="a.j.kelly@durham.ac.uk",
     description="A python wrapper for the GSL integration routines",
     url="https://github.com/AshKelly/pyquad",
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License (GPL)"
     ],
     install_requires=['cython', 'numpy', 'numba'],
     extras_require={
         'test': ['pytest', 'scipy'],
     },
-    ext_modules=[Extension("pyquad",
-                           src_files,
-                           extra_compile_args=link_args,
-                           extra_link_args=link_args,
-                           include_dirs=[np.get_include()],
-                           language='c')],
+    ext_modules=ext_modules,
 )
