1696c1d1e90abf7447f3a4c2bd6528be8b52443c
ashkelly <a.j.kelly@durham.ac.uk>
2019-12-11 10:36:01 +0000

    just some minor formatting changes

--- pyquad/quad.c
+++ pyquad/quad.c
@@ -32,16 +32,20 @@ typedef struct{
 
 
 void _quad(int num_args, double a, double b, void * p, double epsabs,
-    double epsrel, size_t limit, double * result, double * error){
+    double epsrel, size_t limit, double * result, double * error,
+    int integration_method){
 
     gsl_integration_cquad_workspace * w = gsl_integration_cquad_workspace_alloc(limit);
+    //gsl_integration_workspace * w = gsl_integration_workspace_alloc(limit);
+
     gsl_function gfunc;
     gfunc.function = integrand_functions[num_args][0];
     gfunc.params = p;
+
     gsl_integration_cquad(&gfunc, a, b, epsabs, epsrel, w, result, error, &limit);
-    gsl_integration_cquad_workspace_free(w);
-    //gsl_integration_workspace * w = gsl_integration_workspace_alloc(limit);
     //gsl_integration_qags(&gfunc, a, b, epsabs, epsrel, limit, w, result, error);
+
+    gsl_integration_cquad_workspace_free(w);
     //gsl_integration_workspace_free(w);
 }
 
@@ -52,15 +56,18 @@ void _quad_grid(int num_args, int num_grid_args, double a, double b, params ps,
 
     // Set up the integration workspace
     gsl_integration_cquad_workspace * w = gsl_integration_cquad_workspace_alloc(limit);
+    //gsl_integration_workspace * w = gsl_integration_workspace_alloc(limit);
+
     gsl_function gfunc;
     gfunc.function = integrand_functions[num_args][num_grid_args];
     gfunc.params = (void *)&ps;
 
     for(int i=0; i<num; i++){
         ps.grid_args = &grid[i*num_grid_args];
-	//gsl_integration_qags(&gfunc, a, b, epsabs, epsrel, limit, w, &result[i], &error[i]);
-	gsl_integration_cquad(&gfunc, a, b, epsabs, epsrel, w, &result[i], &error[i], &limit);
+	    //gsl_integration_qags(&gfunc, a, b, epsabs, epsrel, limit, w, &result[i], &error[i]);
+	    gsl_integration_cquad(&gfunc, a, b, epsabs, epsrel, w, &result[i], &error[i], &limit);
     }
+
     //gsl_integration_workspace_free(w);
     gsl_integration_cquad_workspace_free(w);
 }
@@ -74,8 +81,10 @@ void * _quad_grid_parallel(void * args){
     // Set up the integration workspace
     //gsl_integration_workspace * w = gsl_integration_workspace_alloc(pargs->limit);
     gsl_integration_cquad_workspace * w = gsl_integration_cquad_workspace_alloc(pargs->limit);
+
     // deactivate default gsl error handler
     gsl_set_error_handler_off();
+
     gsl_function gfunc;
     gfunc.function = integrand_functions[pargs->num_args][pargs->num_grid_args];
     gfunc.params = (void *)&pargs->ps;
@@ -116,8 +125,6 @@ void _quad_grid_parallel_wrapper(int num_args, int num_grid_args, double a,
     cpu_set_t cpus;
     #endif
 
-    //printf("num_per_thread: %i (%i, %i) \n", num_per_thread,
-    //       num, num_threads);
     for(int i=0; i < num_threads; i++){
         // Pass the relevant work
         pargs[i].num_args = num_args;
@@ -131,7 +138,7 @@ void _quad_grid_parallel_wrapper(int num_args, int num_grid_args, double a,
         pargs[i].b = b;
         pargs[i].epsabs = epsabs;
         pargs[i].epsrel = epsrel;
-	pargs[i].status = GSL_SUCCESS;
+        pargs[i].status = GSL_SUCCESS;
 
         // Get the range of integrals for this thread
         pargs[i].lower = num_per_thread * i;
@@ -158,9 +165,6 @@ void _quad_grid_parallel_wrapper(int num_args, int num_grid_args, double a,
         pthread_join(thread[i], NULL);
     }
     for (int i=0; i < num_threads; i++){
-             status[i] = pargs[i].status;		
+        status[i] = pargs[i].status;
     }
-    //}
-    //printf("%d\n",status);
-
 }
